a3068b4122929fcf0a034903a2d3ee060699f617
Andrew Anastasiades <atanasta.aa@gmail.com>
2021-05-22 23:05:20 -0400

    Cleaning submission files

--- app.py
+++ app.py
@@ -43,101 +43,70 @@ def welcome():
         f"/api/v1.0/start/end<br/>"
     )
 
-
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    
     # Create our session (link) from Python to the DB
     session = Session(engine)
-
     """Return a dict of all precipitation data"""
     # Query all precipitation data
     results = dict(session.query(measurement.date, measurement.prcp).all())
-
     session.close()
-    
     return jsonify(results)
 
 @app.route("/api/v1.0/stations")
 def stations():
-    
     # Create our session (link) from Python to the DB
     session = Session(engine)
-
     """Return a list of all stations"""
     # Query all precipitation data
     results = session.query(station.name).group_by(station.name).all()
-
     result_list = list(np.ravel(results))
-
     session.close()
-    
     return jsonify(result_list)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    
     # Create our session (link) from Python to the DB
     session = Session(engine)
-    
     """Return most active station"""
     most_active_station = session.query(measurement.station, func.count(measurement.station)) \
         .group_by(measurement.station) \
         .order_by(func.count(measurement.station).desc()) \
         .all()[0][0]
-    
     """Return a list of all tobs data from most active"""
     results = list(np.ravel(session.query(measurement.tobs).filter(measurement.station==most_active_station).all()))
-    
     session.close()
-    
     return jsonify(results)
 
 @app.route("/api/v1.0/<start>")
 def start(start):
-    
     start_date = dt.datetime.strptime(start, '%Y-%m-%d')
-    
     # Create our session (link) from Python to the DB
     session = Session(engine)
-    
     """Return temps after date"""
     temps_after_start = session.query(func.min(measurement.tobs), 
                                       func.avg(measurement.tobs), 
                                       func.max(measurement.tobs)).filter(measurement.date >= start_date).all()
-    
     results = list(np.ravel(temps_after_start))
-                   
     """Return a list of all tobs data from most active"""
-    
     session.close()
-    
     return jsonify(results)
 
 @app.route("/api/v1.0/<start>/<end>")
 def start_end(start, end):
-    
     start_date = dt.datetime.strptime(start, '%Y-%m-%d')
     end_date = dt.datetime.strptime(end, '%Y-%m-%d')
-    
     # Create our session (link) from Python to the DB
     session = Session(engine)
-    
-    """Return temps after date"""
+    """Return temps between dates"""
     temps_between = session.query(func.min(measurement.tobs), 
                                       func.avg(measurement.tobs), 
                                       func.max(measurement.tobs)
                                      ).filter(measurement.date >= start_date).filter(measurement.date <= end_date).all()
-    
     results = list(np.ravel(temps_between))
-                   
     """Return a list of all tobs data from most active"""
-    
     session.close()
-    
     return jsonify(results)
-
-
 
 
 if __name__ == '__main__':
